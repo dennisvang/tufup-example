e4cdef2ef92a68869208f008a1d90f790b709a73
Dennis van Gerwen <29799340+dennisvang@users.noreply.github.com>
2022-06-15 16:17:20 +0200

    fix app version
    wrong version committed accidentally...

--- src/myapp/settings.py
+++ src/myapp/settings.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 
 # App info
 APP_NAME = 'my_app'
-APP_VERSION = '3.0'
+APP_VERSION = '1.0'
 
 # On Windows 10, a typical location for app data would be %PROGRAMDATA%\MyApp
 # (per-machine), or %LOCALAPPDATA%\MyApp (per-user). Typical app installation
