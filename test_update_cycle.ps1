1b98013e61acd630772ea0866dda180600a1197e
dennisvang <29799340+dennisvang@users.noreply.github.com>
2023-11-10 15:38:22 +0100

    test path existence in ps script

--- test_update_cycle.ps1
+++ test_update_cycle.ps1
@@ -3,8 +3,9 @@
 # https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_scripts
 
 # remove content of temp dir
-$tempdir = "$PSScriptRoot\temp"
-$abort = Read-Host "This will delete everything from '$tempdir'`nHit enter to continue, or any other key to abort"
+$repo_dir = $PSScriptRoot
+$temp_dir = "$repo_dir\temp"
+$abort = Read-Host "This will delete everything from '$temp_dir'`nHit enter to continue, or any other key to abort"
 if ( $abort ) {
     Write-Output "aborted"
     exit
@@ -12,9 +13,12 @@ if ( $abort ) {
 # we could simply remove $tempdir\*, but let's just be a bit more specific
 # to prevent accidental deletion of unrelated files
 # https://learn.microsoft.com/en-us/powershell/scripting/learn/deep-dives/everything-about-arrays
-$subdirs = $("aap") # $("build", "dist", "keystore", "repository")
+$subdirs = $("build", "dist", "keystore", "repository")
 # I think recurse can be used here, despite "known issues"...
 $subdirs | ForEach-Object {
-    Remove-Item "$tempdir\$_" -Recurse -Confirm
-    Write-Output "removed '$tempdir\$_'"
+    $path = "$temp_dir\$_"
+    if (Test-Path $path) {
+        Remove-Item $path -Recurse  # -Confirm
+        Write-Output "removed '$path'"
+    }
 }
